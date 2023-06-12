523f5c30178a49a76f4fad199e48762e575fd754
Viktoria Rakhely <96790600+Rea888@users.noreply.github.com>
2023-06-12 12:52:14 +0200

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,49 @@
+$("a.delete").on("click", function (e) {
+
+    console.log("delete");
+    e.preventDefault();
+
+    if (confirm("Are you sure?")) {
+
+        var frm = $("<form>");
+        frm.attr('method', 'post');
+        frm.attr('action', $(this).attr('href'));
+        frm.appendTo("body");
+        frm.submit();
+    }
+
+});
+
+$.validator.addMethod("dateTime", function (value, element) {
+
+    return (value == "") || !isNaN(Date.parse(value));
+}, "Must be a valid date and time");
+
+$("#formArticle").validate({
+    rules: {
+        title: {
+            required: true
+        },
+        content: {
+            required: true
+        },
+        publisched_at: {
+            dateTime: true
+        }
+
+    }
+});
+
+$("button.publish").on("click", function (e) {
+
+    var id = $(this).data('id');
+    var button = $(this);
+    $.ajax({
+        url: '/article/admin/publish_article.php',
+        type: 'POST',
+        data: {id: id}
+    })
+        .done(function (data) {
+            button.parent().html(data);
+        })
+});
