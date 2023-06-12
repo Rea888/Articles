055e57678ea817bc0c9362f834ef1b381dac3f0b
Viktoria Rakhely <96790600+Rea888@users.noreply.github.com>
2023-06-12 12:38:17 +0200

    Update Category.php

--- classes/Category.php
+++ classes/Category.php
@@ -4,7 +4,7 @@ class Category
 {
 
     public static function getAll($conn)
-    { //static because this method won't be acting upon an idividual article object
+    {
 
         $sql = "SELECT * FROM category ORDER BY name;";
 
